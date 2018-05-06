a4b7a0506a294848c6587179376a9631286289ca
asela <123azeela@gmail.com>
2018-05-06 18:31:44 +0530

    testing player customization

--- src/DefaultPlayer/Overlay/Overlay.js
+++ src/DefaultPlayer/Overlay/Overlay.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import styles from './Overlay.css';
-import PlayArrow from './../Icon/play_arrow.svg';
-import Spin from './../Icon/spin.svg';
-import Report from './../Icon/report.svg';
+import React, { Component } from "react";
+import styles from "./Overlay.css";
+import PlayArrow from "./../Icon/play_arrow.svg";
+import Spin from "./../Icon/spin.svg";
+import Report from "./../Icon/report.svg";
 
 export default class Overlay extends Component {
-    renderContent () {
-        const {
-            error,
-            paused,
-            loading
-        } = this.props;
+    renderContent() {
+        const { error, paused, loading } = this.props;
         const iconProps = {
             className: styles.icon,
             height: 40,
             width: 40,
-            fill: '#fff'
+            fill: "#fff"
         };
         if (error) {
             return (
@@ -38,15 +34,11 @@ export default class Overlay extends Component {
         }
     }
 
-    render () {
+    render() {
         const { className, onClick } = this.props;
         return (
-            <div className={[
-                styles.component,
-                className
-            ].join(' ')}
-            onClick={onClick}>
-                { this.renderContent() }
+            <div className={[styles.component, className].join(" ")}>
+                {this.renderContent()}
             </div>
         );
     }
