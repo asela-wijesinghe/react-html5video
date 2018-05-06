bac14ed4bc5e1f30ddf38830fe3d02df4df3ac6f
asela <123azeela@gmail.com>
2018-05-06 18:35:34 +0530

    testing the changes

--- src/DefaultPlayer/PlayPause/PlayPause.js
+++ src/DefaultPlayer/PlayPause/PlayPause.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import styles from './PlayPause.css';
-import PlayArrow from './../Icon/play_arrow.svg';
-import Pause from './../Icon/pause.svg';
+import React from "react";
+import styles from "./PlayPause.css";
+import PlayArrow from "./../Icon/play_arrow.svg";
+import Pause from "./../Icon/pause.svg";
 
-export default ({ onClick, paused, className, ariaLabelPlay, ariaLabelPause }) => {
+export default ({
+    onClick,
+    paused,
+    className,
+    ariaLabelPlay,
+    ariaLabelPause
+}) => {
     return (
-        <div className={[
-            styles.component,
-            className
-        ].join(' ')}>
+        <div className={[styles.component, className].join(" ")}>
             <button
                 className={styles.button}
                 onClick={onClick}
-                aria-label={ paused
-                    ? ariaLabelPlay
-                    : ariaLabelPause }
-                type="button">
-                { paused
-                    ? <PlayArrow
-                        className={styles.icon}
-                        fill="#fff" />
-                    : <Pause
-                        className={styles.icon}
-                        fill="#fff" /> }
+                aria-label={paused ? ariaLabelPlay : ariaLabelPause}
+                type="button"
+            >
+                {paused ? (
+                    <PlayArrow className={styles.icon} fill="#ff663c" />
+                ) : (
+                    <Pause className={styles.icon} fill="#ff663c" />
+                )}
             </button>
         </div>
     );
